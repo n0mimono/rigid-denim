2ac81c4d2ec3699b973b83fa679811548ad0a9ff
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-07-26 17:30:40 +0900

    refactor

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class Utility {
+
+	public static IEnumerator Update(this object obj, Action action, float seconds = 0f) {
+		WaitForSeconds waitSecs = new WaitForSeconds(seconds);
+
+		while (true) {
+			action();
+			yield return waitSecs;
+		}
+	}
+
+	public static void StartBy(this IEnumerator routine, MonoBehaviour behav) {
+		behav.StartCoroutine(routine);
+	}
+
+	public static Vector3 FreesePosition(this Transform trans) {
+		return new Vector3(trans.position.x, 0f, trans.position.z);
+	}
+
+	public static Vector3 FreeseAngles(this Transform trans) {
+		return new Vector3(0f, trans.eulerAngles.z, 0f);
+	}
+
+}
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -40,28 +40,3 @@ public class Vehicle : MonoBehaviour {
 	}
 
 }
-
-public static class Utility {
-
-	public static IEnumerator Update(this object obj, Action action, float seconds = 0f) {
-		WaitForSeconds waitSecs = new WaitForSeconds(seconds);
-
-		while (true) {
-			action();
-			yield return waitSecs;
-		}
-	}
-
-	public static void StartBy(this IEnumerator routine, MonoBehaviour behav) {
-		behav.StartCoroutine(routine);
-	}
-
-	public static Vector3 FreesePosition(this Transform trans) {
-		return new Vector3(trans.position.x, 0f, trans.position.z);
-	}
-
-	public static Vector3 FreeseAngles(this Transform trans) {
-		return new Vector3(0f, trans.eulerAngles.z, 0f);
-	}
-
-}
