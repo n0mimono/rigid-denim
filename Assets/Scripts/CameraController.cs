ba525e4b64d329b54488def7625f723361117f95
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-07-27 13:50:02 +0900

    camrea

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class CameraController : MonoBehaviour, IDragHandler {
+	public float dragScale;
+
+	private Transform camTrans;
+	private Vector3 Angles {
+		set {
+			if (camTrans == null) camTrans = Camera.main.transform;
+			camTrans.localEulerAngles = value;
+		}
+		get {
+			if (camTrans == null) camTrans = Camera.main.transform;
+			return camTrans.localEulerAngles;
+		}
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Vector3 ang = Angles;
+		ang.y += eventData.delta.x * dragScale;
+		Angles = ang;
+	}
+
+}
