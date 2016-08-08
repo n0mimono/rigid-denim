bb83125a76a8f9fa477103d09b3ebf1c203a177f
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2016-08-08 11:59:08 +0900

    Helperのみ追加

---
+++ Assets/Scripts/CollisionPoint/MeshTriggerHelper.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshTriggerHelper : MonoBehaviour {
+  private Vector3[] verteces;
+  private Transform trans;
+
+  void Awake() {
+    MeshCollider col = GetComponent<MeshCollider> ();
+    verteces = col.sharedMesh.vertices;
+    trans = transform;
+  }
+
+  public Vector3 ClosestPointOnMesh(Vector3 point) {
+    Matrix4x4 l2w = trans.localToWorldMatrix;
+
+    Vector3 minPoint = trans.position;
+    float minDist2 = Vector3.SqrMagnitude(minPoint - point);
+    for (int i = 0; i < verteces.Length; i++) {
+      Vector3 v = l2w.MultiplyPoint (verteces [i]);
+      float dist2 = Vector3.SqrMagnitude (v - point);
+      if (dist2 < minDist2) {
+        minPoint = v;
+        minDist2 = dist2;
+      }
+    }
+    return minPoint;
+  }
+
+}
---
+++ Assets/Scripts/CollisionPoint/MeshTriggerHelperTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshTriggerHelperTest : MonoBehaviour {
+  public Transform point;
+
+  void OnTriggerStay(Collider collider) {
+    MeshTriggerHelper helper = collider.GetComponent<MeshTriggerHelper> ();
+    if (helper != null) {
+      point.position = helper.ClosestPointOnMesh (transform.position);
+    }
+  }
+
+}
